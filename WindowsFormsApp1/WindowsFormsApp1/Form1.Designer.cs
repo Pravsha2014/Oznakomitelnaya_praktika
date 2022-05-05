3ab43c13cd384a187a34e05c5b041a1eb6083ae5
Pravsha2014 <84060074+Pravsha2014@users.noreply.github.com>
2022-05-05 05:14:28 +0500

    First commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.переместитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.этоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кудаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 71);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1223, 467);
+            this.splitContainer1.SplitterDistance = 407;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(407, 467);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Папка");
+            this.imageList1.Images.SetKeyName(1, "Файл");
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(812, 467);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название";
+            this.columnHeader1.Width = 210;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Автор";
+            this.columnHeader2.Width = 139;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Дата последнего изменения";
+            this.columnHeader3.Width = 188;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Размер";
+            this.columnHeader4.Width = 88;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(597, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалитьToolStripMenuItem,
+            this.переместитьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(147, 48);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // переместитьToolStripMenuItem
+            // 
+            this.переместитьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.этоToolStripMenuItem,
+            this.кудаToolStripMenuItem});
+            this.переместитьToolStripMenuItem.Name = "переместитьToolStripMenuItem";
+            this.переместитьToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.переместитьToolStripMenuItem.Text = "Переместить";
+            // 
+            // этоToolStripMenuItem
+            // 
+            this.этоToolStripMenuItem.Name = "этоToolStripMenuItem";
+            this.этоToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.этоToolStripMenuItem.Text = "Это";
+            this.этоToolStripMenuItem.Click += new System.EventHandler(this.этоToolStripMenuItem_Click);
+            // 
+            // кудаToolStripMenuItem
+            // 
+            this.кудаToolStripMenuItem.Name = "кудаToolStripMenuItem";
+            this.кудаToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.кудаToolStripMenuItem.Text = "Куда";
+            this.кудаToolStripMenuItem.Click += new System.EventHandler(this.кудаToolStripMenuItem_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(791, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Адрес";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1018, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Поиск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 536);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem переместитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem этоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem кудаToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal; 
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public List<string> lastpath = new List<string>();
+        public bool a = true;
+        public string moveTo = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                FolderBrowserDialog FBD = new FolderBrowserDialog();
+                if (FBD.ShowDialog() == DialogResult.OK)
+                {
+                    string path = FBD.SelectedPath;
+                    PopulateTreeView(path);
+                }
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message, "Ошибка");
+            }
+        }
+        
+        public void Paths(string paths)
+        {
+            lastpath.Add(paths);
+        }
+        private void PopulateTreeView(string path)
+        {
+            TreeNode rootNode;
+            DirectoryInfo info = new DirectoryInfo(path);
+            
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                treeView1.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs,
+            TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            listView1.Items.Clear();
+            fill(nodeDirInfo);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        void fill(DirectoryInfo nodeDirInfo)
+        {
+            ListViewItem item = null;
+            ListViewItem.ListViewSubItem[] subItems;
+            
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    {new ListViewItem.ListViewSubItem(item, GetFolderOwner(dir)),
+                     new ListViewItem.ListViewSubItem(item, dir.LastWriteTime.ToShortDateString()),
+                     new ListViewItem.ListViewSubItem(item, DirSize(dir).ToString() + " байт")};
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, GetFileOwner(file)),
+                 new ListViewItem.ListViewSubItem(item, file.LastWriteTime.ToShortDateString()),
+                 new ListViewItem.ListViewSubItem(item, file.Length.ToString() + " байт"),};
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+            if (a)
+            {
+                Paths(nodeDirInfo.FullName);
+            }
+            textBox1.Text = nodeDirInfo.FullName;
+        }
+        static string GetFolderOwner(DirectoryInfo folder)
+        {
+            var ds = folder.GetAccessControl(AccessControlSections.Owner);
+            var nta = ds.GetOwner(typeof(NTAccount));
+            string folder_owner = nta.ToString();
+            folder_owner = folder_owner.Split('\\')[1];
+            return folder_owner;
+        }
+
+        static string GetFileOwner(FileInfo fileinfo)
+        {
+            FileSecurity fileSecurity = fileinfo.GetAccessControl();
+            IdentityReference identityReference = fileSecurity.GetOwner(typeof(NTAccount));
+            string owner = identityReference.Value;
+            owner = owner.Split('\\')[1];
+            return owner;
+        }
+
+        static long DirSize(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Sum(fi => fi.Length) +
+                   dir.GetDirectories().Sum(di => DirSize(di));
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+                
+                if (textBox1.Text != "" && dir.Exists)
+                {
+                    PopulateTreeView(textBox1.Text);
+                    listView1.Items.Clear();
+                    treeView1.Nodes[0].Remove();
+                    fill(dir);
+                }
+                else
+                {
+                    MessageBox.Show("Пути не существует", "Ошибка");
+                }
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message, "Ошибка");
+            }
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            a = false;
+            if(lastpath.Count != 0)
+            {
+                DirectoryInfo dir = new DirectoryInfo(lastpath[lastpath.Count - 1]);
+                lastpath.RemoveAt(lastpath.Count - 1);
+                PopulateTreeView(dir.FullName);
+                listView1.Items.Clear();
+                treeView1.Nodes[0].Remove();
+                fill(dir);
+                a = true;
+            }
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string name = listView1.SelectedItems[0].Text;
+            string del_file = lastpath[lastpath.Count-1] + @"\" + name;
+            FileInfo fInfo = new FileInfo(del_file);
+            fInfo.Delete();
+            listView1.Items.Remove(listView1.SelectedItems[0]);
+        }
+
+        private void этоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string name = listView1.SelectedItems[0].Text;
+            moveTo = lastpath[lastpath.Count - 1] + @"\" + name;
+        }
+
+        private void кудаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string move = listView1.SelectedItems[0].Text;
+            DirectoryInfo dir = new DirectoryInfo(lastpath[lastpath.Count - 1] + @"\" + move + @"\" + moveTo.Substring(moveTo.LastIndexOf('\\')));
+            new FileInfo(moveTo).MoveTo(dir.FullName);
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(MousePosition);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string search_file = textBox2.Text;
+                string[] allFoundFiles = Directory.GetFiles(lastpath[lastpath.Count - 1], search_file, SearchOption.AllDirectories);
+                listView1.Items.Clear();
+                foreach (string file in allFoundFiles)
+                {
+                    PopulateTreeView(file.Remove(file.LastIndexOf('\\')));
+                    fill(new DirectoryInfo(file.Remove(file.LastIndexOf('\\'))));
+
+                }
+                ListView.ListViewItemCollection toFindColection = listView1.Items;
+                for (int i = 0; i < toFindColection.Count; i++)
+                {
+                    if (toFindColection[i].Text == search_file)
+                    {
+                        listView1.Select();
+                        toFindColection[i].Selected = true;
+                        break;
+                    }
+                }
+                treeView1.Nodes[0].Remove();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Ошибка");
+            }
+        }
+    }
+}
+
